597bf424b94873da955c66ebcd9ecaff825ba019
John Dominic Ragudo <jsragudo@gmail.com>
2025-03-01 09:34:25 +0800

    Initial commit - Welcome script for the path of the Dev Tester

---
+++ welcome.py
@@ -0,0 +1,6 @@
+# This program welcomes the user to the Path of the Dev Tester and asks the user for their name
+
+print('Welcome to the Path of the Dev Tester')
+print('What is your name?')  # ask for user's name
+myName = input()
+print('It is good to meet you, ' + myName + ', Welcome to the Path of the Dev Tester!')
